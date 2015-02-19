e4bf548e5c35a1d0c3562c42674ee2e215455e72
validol <validol@marat.lt>
2015-02-19 05:55:39 +0200

    Update Category.java

--- Category.java
+++ Category.java
@@ -6,7 +6,9 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class Category {
-    // category class
+    /*
+    * New comment
+    */
 
     public String categ_name    = "categ_name";
     public String categ_varname = "categ_varname";
